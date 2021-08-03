a8bf4cafb5e7423055e442d883e64dcec6a0f7fd
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-03 16:21:30 -0400

    set up the constructor method

--- project-lifecycle/src/App.js
+++ project-lifecycle/src/App.js
@@ -1,3 +1,17 @@
 import "./App.css";
 import React from "react";
 import axios from "axios";
+
+class App extends React.Component {
+  //Constructor method for creating an object with a class / setting state
+
+  constructor() {
+    super();
+    this.state = {
+      userCards: {},
+      followers: [],
+    };
+  }
+
+  //Setting up data for API pull for github
+}
